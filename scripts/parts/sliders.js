eb725c2e120c7efacf239671e6b9d3831e62865f
Adelia <adelgants@gmail.com>
2020-07-25 23:55:54 +0300

    Updates

---
+++ scripts/parts/sliders.js
@@ -0,0 +1,1 @@
+$(document).ready((function(){$(".header-slider").slick({slidesToShow:1,slidesToScroll:1,arrows:!0,asNavFor:".header-nav"}),$(".header-nav").slick({slidesToShow:3,slidesToScroll:1,asNavFor:".header-slider",centerMode:!0,focusOnSelect:!0,vertical:!0})})),$(document).ready((function(){$(".main-slider").slick({slidesToShow:3,centerMode:!0})}));
